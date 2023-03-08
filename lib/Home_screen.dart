2d19a8ca0de86cc6b6536b4874bca3c98fea7fa4
Saeed Sabra <saeedsabra565@gmail.com>
2023-03-08 09:19:40 +0200

    first commit

---
+++ lib/Home_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:flutter/material.dart';
+
+class Home_screen extends StatefulWidget {
+  const Home_screen({super.key});
+
+  @override
+  State<Home_screen> createState() => _Home_screenState();
+}
+
+class _Home_screenState extends State<Home_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/opening.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class opening extends StatefulWidget {
+  const opening({super.key});
+
+  @override
+  State<opening> createState() => _openingState();
+}
+
+class _openingState extends State<opening> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
